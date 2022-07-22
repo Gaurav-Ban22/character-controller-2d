11bcbaaf7094474a327375e94d80258531088d08
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-07-22 14:31:04 -0700

    initial

---
+++ playerControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+
+    public float isOnGround;
+    public RigidBody2D rb;
+    
+}
